65b8748c2fca6311d5766b23b52c2c23d3dd25d3
Syncrhonous <syncmac@Syncrhonouss-Mac-mini.local>
2019-09-14 14:13:33 +0600

    1.0.1

--- src/__tests__/Greeter.test.ts
+++ src/__tests__/Greeter.test.ts
@@ -1,8 +1,8 @@
-import { Greeter, MyName } from '../index';
-test('My Greeter', () => {
-  expect(Greeter('Carl')).toBe('Hello Carl');
+import { Greeter, MyName } from "../index";
+test("My Greeter", () => {
+  expect(Greeter("Carl")).toBe("Hello Carl");
 });
 
-test('My Greeter', () => {
-  expect(MyName('Carl')).toBe('Your Name Is: Carl');
+test("My Greeter", () => {
+  expect(MyName("Carl")).toBe("Your Name Is: Carl");
 });

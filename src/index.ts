c4132997a195d8dfc79b60ee19b24cd7b923b5d9
Syncrhonous <syncmac@Syncrhonouss-Mac-mini.local>
2019-09-14 14:44:55 +0600

    1.0.6

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import TestCl from "./TestClass"
+import TestCl from "./TestClass";
 const Greeter = (name: string) => `Hello ${name}`;
 const MyName = (name: string) => `Your Name Is: ${name}`;
-export  {TestCl,Greeter,MyName};
+export { TestCl, Greeter, MyName };

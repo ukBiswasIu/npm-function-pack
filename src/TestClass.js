a3955548cb75bdc4c04881549cfe637812aae24b
Syncrhonous <syncmac@Syncrhonouss-Mac-mini.local>
2019-09-14 14:39:25 +0600

    changes

---
+++ src/TestClass.js
@@ -0,0 +1,5 @@
+export default class TestClass {
+  hello(name) {
+    return `Hey ${name}`;
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,3 @@
+
 export const Greeter = (name: string) => `Hello ${name}`;
 export const MyName = (name: string) => `Your Name Is: ${name}`;
-
-export class TestClass {
-  public disPlay = (name: string) => {
-    `DisPlay Name Is: ${name}`;
-  };
-}

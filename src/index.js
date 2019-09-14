4a5f43db568b2712acea05b0128e2ca3a0eb3601
Syncrhonous <syncmac@Syncrhonouss-Mac-mini.local>
2019-09-14 14:49:58 +0600

    changes

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import TestCl from "./TestClass";
+const Greeter = (name) => `Hello ${name}`;
+const MyName = (name) => `Your Name Is: ${name}`;
+export { TestCl, Greeter, MyName };
--- src/index.ts
+++
@@ -1,4 +0,0 @@
-import TestCl from "./TestClass";
-const Greeter = (name: string) => `Hello ${name}`;
-const MyName = (name: string) => `Your Name Is: ${name}`;
-export { TestCl, Greeter, MyName };
